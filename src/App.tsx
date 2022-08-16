78c25dd2df6f88daa8eacef160f885de1cc8aa3e
Rahul Shial <rahul.shial@gmail.com>
2022-08-16 16:01:07 -0600

    Page Layout, initial state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
-import React, { useState } from 'react'
-import Square from '@components/Square';
+import React, { useState, useEffect } from 'react'
+import Square from './components/Square';
 
 function App() {
-  const INITIAL_GAME_STATE = ["", "", "", "", "", "", "", "", ""];
+  const INITIAL_GAME_STATE = new Array(9).fill("");
   const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
+  const [currentPlayer, setCurrentPlayer] = useState("X")
+
+  useEffect(() => {
+  changePlayer()
+  }, [gameState])
+  
+  const changePlayer = () => {
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X")
+  }
+  const handleCellClick = (event: any) => {
+    const cellIndex = Number(event.target.getAttribute('data-cell-index'))
+    if(!gameState[cellIndex]) {
+      const newValues = [...gameState]
+      newValues[cellIndex] = currentPlayer
+      setGameState(newValues)
+    }
+  }
 
   return (
     <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
       <h1 className="text-center text-5xl mb-4 font-display text-white">Tic Tac Toe</h1>
       <div>
         <div className="grid grid-cols-3 gap-3 mx-auto w-96">
-          {INITIAL_GAME_STATE.map((player, index) => (
-            <Square index={index} player={player}/>
+          {gameState.map((player, index) => (
+            <Square key={index} {...{index, player}} onClick={handleCellClick}/>
           ))}
         </div>
       </div>
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -3,13 +3,27 @@ import React from 'react'
 interface Props {
   index: number,
   player: string,
+  onClick(event: any): void 
 }
-const Square = ({index, player}: Props) => {
+const Square = ({index, player, onClick}: Props) => {
+  const scale = player ? "scale-100" : "scale-0"
+  const textColor = player === 'X' ? "text-yellow-200" : "text-fuchsia-300"
+  const hoverStyle = "transition duration-500 hover:scale-105 transform"
+  const mainStyle = "h-36 border-solid border-4 border-slate-200 font-display text-7xl flex justify-center items-center cursor-pointer"
+  const newPlayer = `transform transition-all duration-150 ease-out ${scale} ${textColor}`
+
   return (
     <div 
-      key={index} 
-      className="h-36 border-solid border-4 border-slate-200 font-display text-7xl justify-center items-center cursor-pointer">
-      {player}
+      data-cell-index={index} 
+      className={`${mainStyle} ${hoverStyle}`}
+      {...{ onClick }}
+    >
+      <span
+        data-cell-index={index}
+        className={newPlayer}
+      >
+        {player}
+      </span>
     </div>
   )
 }
